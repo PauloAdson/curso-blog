71718e89697da632c940dc01f09425365e8b6d05
PauloAdson <paulo.alves200581@gmail.com>
2025-05-01 18:44:47 -0300

    aprendendo sobre Suspense

---
+++ src/components/SpinLoader/SpinLoader.tsx
@@ -0,0 +1,19 @@
+type SpinLoaderProps = {
+  className?: string;
+};
+
+export const SpinLoader = ({ className = '' }: SpinLoaderProps) => {
+  const classes = `flex items-center justify-center ${className}`;
+
+  return (
+    <div className={classes}>
+      <div
+        className="w-10 h-10 border-5
+       border-t-transparent
+        border-slate-900
+        rounded-full
+        animate-spin"
+      ></div>
+    </div>
+  );
+};
