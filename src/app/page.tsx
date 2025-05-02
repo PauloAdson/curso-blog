948bef64a1ca7accfb76aaa0949b77bef55537e2
PauloAdson <paulo.alves200581@gmail.com>
2025-05-02 19:37:13 -0300

    criando o container do site e estilizando

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import { Container } from '@/components/container';
 import { PostsList } from '@/components/PostsList';
 import { SpinLoader } from '@/components/SpinLoader/SpinLoader';
 import { Suspense } from 'react';
 
 export default async function HomePage() {
   return (
-    <div>
+    <Container>
       <header>
         <h1 className="text-6xl font-bold text-center py-8">Aqui é a HEADER</h1>
       </header>
@@ -14,6 +15,6 @@ export default async function HomePage() {
       <footer>
         <p className="text-6xl font-bold text-center py-8">Footer</p>
       </footer>
-    </div>
+    </Container>
   );
 }
