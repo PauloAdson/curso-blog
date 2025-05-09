b789f3e3817ecbe870c3080fc57cffe92cccd395
PauloAdson <paulo.alves200581@gmail.com>
2025-05-09 17:34:56 -0300

    movendo alguns componentes para o layout, e adicionando um título padrão para a página

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,14 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import React from 'react';
+import { Container } from '@/components/container';
+import { Header } from '@/components/Header';
 
 export const metadata: Metadata = {
-  title: 'The blog - Este é um blog com Next.js',
+  title: {
+    default: 'The blog - Este é um blog com Next.js',
+    template: '%s | The Blog',
+  },
   description: 'Descrição do blog',
 };
 
@@ -14,7 +19,15 @@ type RootLayoutProps = {
 export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
   return (
     <html lang='pt-BR' className='dark'>
-      <body>{children}</body>
+      <body>
+        <Container>
+          <Header />
+          {children}
+          <footer>
+            <p className='text-6xl font-bold text-center py-8'>Footer</p>
+          </footer>
+        </Container>
+      </body>
     </html>
   );
 }
