29d47ed63b567f3e31b144a9bfedc52e937f142e
PauloAdson <paulo.alves200581@gmail.com>
2025-05-11 12:35:10 -0300

    adicionando uma variável onde é possível encontrar os posts pelo slug

--- src/repositories/post/post-repository.ts
+++ src/repositories/post/post-repository.ts
@@ -3,5 +3,5 @@ import { PostModel } from '@/models/post/post-model';
 export interface PostRepository {
   findAllPublic(): Promise<PostModel[]>;
   findById(id: string): Promise<PostModel>;
-  findById(slug: string): Promise<PostModel>;
+  findBySlug(slug: string): Promise<PostModel>;
 }
