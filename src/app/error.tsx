d4dd2cda6f9aba9f4b5044cb2e70b8d3a0aedd77
PauloAdson <paulo.alves200581@gmail.com>
2025-05-13 17:28:41 -0300

    alterando para use client

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import ErrorMessage from '@/components/ErrorMessage';
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 
 type RootErrorPageProps = {
   error: Error;
